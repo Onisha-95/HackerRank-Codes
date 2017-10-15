4c38ad0be00db700bb9032172c7c402d07c8439d
onisha-95 <aktar15-299@diu.edu.bd>
2017-10-15 14:45:18 +0600

    Java End of file added

---
+++ Java_EndOfFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+             Scanner cin=new Scanner(System.in);
+	int l=1;
+	for( ;cin.hasNext();l++){
+		System.out.println(String.valueOf(l)+" "+cin.nextLine());
+}
+    }
+}
